5036b3e63647b8805487d6798c8ff1d0db341723
mrjamsss <joraldmasilsevilla0806@gmail.com>
2026-05-19 19:04:34 +0800

    fix: implement flash message partial view

--- App/views/partials/message.php
+++ App/views/partials/message.php
@@ -6,14 +6,16 @@ use Framework\Session;
 
 <?php $successMessage = Session::getFlashMessage('success_message'); ?>
 <?php if ($successMessage !== null): ?>
-    <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 my-10 rounded relative mb-4">
-        <?= $successMessage ?>
+    <div class="flash-message flash-success my-6">
+        <i class="fa-solid fa-circle-check mr-3 text-xl"></i>
+        <span><?= $successMessage ?></span>
     </div>
 <?php endif; ?>
 
 <?php $errorMessage = Session::getFlashMessage('error_message'); ?>
 <?php if ($errorMessage !== null): ?>
-    <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 my-10 rounded relative mb-4">
-        <?= $errorMessage ?>
+    <div class="flash-message flash-error my-6">
+        <i class="fa-solid fa-circle-exclamation mr-3 text-xl"></i>
+        <span><?= $errorMessage ?></span>
     </div>
 <?php endif; ?>
